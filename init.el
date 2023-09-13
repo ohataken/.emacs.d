3789069f496874de0591c1097941d782723c104f
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2023-09-13 16:05:50 +0900

    update

--- init.el
+++ init.el
@@ -68,7 +68,9 @@
   :config
   (smooth-scrolling-mode t))
 
-(use-package magit)
+(use-package magit
+  :init
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
 (use-package swiper
   :config
